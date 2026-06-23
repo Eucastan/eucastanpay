5454bfd0b418913ee01b1c1893e26cf587391b54
Stanley Emeh <stanemeh808@gmail.com>
2026-06-23 16:51:58 +0100

    User service configuration

--- services/user/config/config.go
+++ services/user/config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
-	"errors"
+	commonconfig "github.com/Eucastan/eucastanpay/common/pkg/config"
+	"strings"
+
 	"github.com/spf13/viper"
 )
 
@@ -12,7 +14,7 @@ type Config struct {
 	GRPCPort    string `mapstructure:"GRPC_PORT"`
 	ServiceName string `mapstructure:"SERVICE_NAME"`
 	Version     string `mapstructure:"VERSION"`
-	Redis       Redis
+	Redis       Redis  `mapstructure:",squash"`
 	EmailAPIKey string `mapstructure:"EMAIL_API_KEY"`
 	AppEmail    string `mapstructure:"APP_EMAIL"`
 	FromName    string `mapstructure:"FROM_NAME"`
@@ -26,24 +28,37 @@ type Redis struct {
 	DB       int    `mapstructure:"REDIS_DB"`
 }
 
-type KafkaConfig struct{ Brokers []string }
+type KafkaConfig struct {
+	Brokers []string `mapstructure:"KAFKA_BROKERS"`
+}
 
 func Load() (*Config, error) {
-	viper.SetConfigFile(".env")
-	viper.AutomaticEnv()
-
-	err := viper.ReadInConfig()
-	if err != nil {
-		return nil, err
-	}
+	commonconfig.Init()
 
 	var cfg Config
 	if err := viper.Unmarshal(&cfg); err != nil {
 		return nil, err
 	}
 
-	if len(cfg.JWTSecret) < 32 {
-		return nil, errors.New("invalid JWT Secret")
+	brokers := viper.GetString("KAFKA_BROKERS")
+	if brokers != "" {
+		cfg.Kafka.Brokers = strings.Split(brokers, ",")
+	}
+
+	if err := commonconfig.RequireString("DSN", cfg.Dsn); err != nil {
+		return nil, err
+	}
+
+	if err := commonconfig.RequireString("HTTP_PORT", cfg.HTTPPort); err != nil {
+		return nil, err
+	}
+
+	if err := commonconfig.RequireString("GRPC_PORT", cfg.GRPCPort); err != nil {
+		return nil, err
+	}
+
+	if err := commonconfig.RequireMinLength("JWT_SECRET", cfg.GRPCPort, 32); err != nil {
+		return nil, err
 	}
 
 	return &cfg, nil
