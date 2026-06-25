6e9294f7fc3a2de74e32e4d78a014e45ecf61519
Stanley Emeh <stanemeh808@gmail.com>
2026-06-25 15:27:59 +0100

    Added graceful shutdown

--- services/transfer/cmd/transfer/main.go
+++ services/transfer/cmd/transfer/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
@@ -10,20 +11,25 @@ import (
 
 	"github.com/Eucastan/eucastanpay/common/idempotency"
 	"github.com/Eucastan/eucastanpay/common/pkg/events"
+	"github.com/Eucastan/eucastanpay/common/pkg/grpc/interceptor"
+	"github.com/Eucastan/eucastanpay/common/pkg/healthcheck"
 	"github.com/Eucastan/eucastanpay/common/pkg/kafka/consumer"
 	"github.com/Eucastan/eucastanpay/common/pkg/kafka/producer"
 	"github.com/Eucastan/eucastanpay/common/pkg/logger"
 	"github.com/Eucastan/eucastanpay/common/pkg/middleware"
+	"github.com/Eucastan/eucastanpay/common/proto/transfer"
 	"github.com/Eucastan/eucastanpay/services/transfer/config"
 	"github.com/Eucastan/eucastanpay/services/transfer/internal/api"
 	"github.com/Eucastan/eucastanpay/services/transfer/internal/api/handler"
 	"github.com/Eucastan/eucastanpay/services/transfer/internal/eventhandler"
 	"github.com/Eucastan/eucastanpay/services/transfer/internal/grpc/clients"
+	"github.com/Eucastan/eucastanpay/services/transfer/internal/grpc/server"
 	"github.com/Eucastan/eucastanpay/services/transfer/internal/infra/database"
 	"github.com/Eucastan/eucastanpay/services/transfer/internal/repository/postgres"
 	"github.com/Eucastan/eucastanpay/services/transfer/internal/usecase/service"
 	"github.com/Eucastan/eucastanpay/services/transfer/internal/worker"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
 )
 
 func main() {
@@ -45,7 +51,10 @@ func main() {
 	publisher := producer.NewPublisher(cfg.Kafka.Brokers)
 	defer publisher.Close()
 
-	go worker.StartOutboxWorker(context.Background(), db.DB, publisher, log)
+	appCtx, appCancel := context.WithCancel(context.Background())
+	defer appCancel()
+
+	go worker.StartOutboxWorker(appCtx, db.DB, publisher, log)
 	idempotencyStore := idempotency.NewPostgresStore()
 
 	consumerInit := consumer.NewConsumer(cfg.Kafka.Brokers, "transfer-group", log)
@@ -112,7 +121,7 @@ func main() {
 		),
 	)
 
-	consumerInit.Start(context.Background())
+	consumerInit.Start(appCtx)
 
 	// Initialize gRPC Clients
 	allClients, err := clients.Init(cfg, log)
@@ -125,10 +134,21 @@ func main() {
 	transferHandler := handler.NewTransferHandler(transferUC)
 	go transferUC.RecoverStuckTransfers(context.Background())
 
+	// Health check init
+	healthChecker := healthcheck.NewHealthChecker("transfer-service", cfg.Version, log)
+	healthChecker.SetDatabase(db.DB)
+	healthChecker.SetKafkaProducer(publisher)
+	healthChecker.AddGRPCClient("account-service", allClients.ConnAccount)
+
 	r := gin.Default()
 	mw := middleware.New(log, cfg.JWTSecret)
 	r.Use(mw.Logger(), mw.Recovery(), mw.Auth())
 	r.Use(middleware.CorrelationMiddleware())
+
+	r.GET("/health", healthChecker.Health)
+	r.GET("/live", healthChecker.Liveness)
+	r.GET("/ready", healthChecker.Readiness)
+
 	api.NewRouter(r, transferHandler)
 
 	httpSrv := &http.Server{
@@ -138,7 +158,17 @@ func main() {
 		WriteTimeout: 15 * time.Second,
 	}
 
-	// Graceful Shutdown
+	listenAddr, err := net.Listen("tcp", ":"+cfg.GRPCPort)
+	if err != nil {
+		log.WithError(err).Error("failed to connect to gRPC server")
+	}
+
+	grpcServer := grpc.NewServer(
+		grpc.UnaryInterceptor(interceptor.AuthInterceptor(cfg.JWTSecret)),
+	)
+	srv := server.NewTransferServiceServer(transferUC)
+	transfer.RegisterTransferServiceServer(grpcServer, srv)
+
 	go func() {
 		if err := httpSrv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.WithError(err).Error("HTTP server error")
@@ -147,13 +177,31 @@ func main() {
 
 	log.Infof("Transfer Service started successfully on port %s", cfg.HTTPPort)
 
+	go func() {
+		log.Infof("gRPC server listening on %s", cfg.GRPCPort)
+		if err := grpcServer.Serve(listenAddr); err != nil {
+			log.WithError(err).Error("gRPC server error")
+		}
+	}()
+
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	<-quit
 
 	log.Info("Shutting down Transfer Service...")
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
 
-	httpSrv.Shutdown(ctx)
+	appCancel()
+
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer shutdownCancel()
+
+	if err := consumerInit.Close(); err != nil {
+		log.WithError(err).Error("Transfer service consumer shutdown error")
+	}
+
+	if err := httpSrv.Shutdown(shutdownCtx); err != nil {
+		log.WithError(err).Error("Transfer service shutdown error")
+	}
+
+	grpcServer.GracefulStop()
 }
