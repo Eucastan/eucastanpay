8de74770ec39e725fd7cf4dc30da047c3c5584db
Stanley Emeh <stanemeh808@gmail.com>
2026-07-06 23:01:19 +0100

    Added structurd logging

--- services/audit/internal/api/router.go
+++ services/audit/internal/api/router.go
@@ -1,12 +1,15 @@
 package api
 
 import (
+	"github.com/Eucastan/eucastanpay/common/pkg/middleware"
+	"github.com/Eucastan/eucastanpay/services/audit/config"
 	"github.com/Eucastan/eucastanpay/services/audit/internal/api/handler"
 	"github.com/gin-gonic/gin"
 )
 
-func NewRouter(r *gin.Engine, h *handler.AuditHandler) {
+func NewRouter(r *gin.Engine, h *handler.AuditHandler, cfg *config.Config) {
 	v1 := r.Group("/api/v1")
+	v1.Use(middleware.Auth(cfg.JWTSecret))
 	{
 		v1.GET("/audit/search", h.Search)
 	}
