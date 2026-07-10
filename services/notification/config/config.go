880dbcd34e15cabb603ef03dad4e622f05919a3c
Stanley Emeh <stanemeh808@gmail.com>
2026-07-10 23:03:33 +0100

    Notification service configuration

---
+++ services/notification/config/config.go
@@ -0,0 +1,63 @@
+package config
+
+import (
+	commonconfig "github.com/Eucastan/eucastanpay/common/pkg/config"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Dsn         string
+	JWTSecret   string
+	HTTPPort    string
+	GRPCPort    string
+	ServiceName string
+	Version     string
+	Redis       Redis `mapstructure:",squash"`
+	EmailAPIKey string
+	AppEmail    string
+	FromName    string
+	Kafka       KafkaConfig
+	LogLevel    string
+	GinMode     string
+}
+
+type Redis struct {
+	Addr     string `mapstructure:"REDIS_ADDR"`
+	Password string `mapstructure:"REDIS_PASSWORD"`
+	DB       int    `mapstructure:"REDIS_DB"`
+}
+
+type KafkaConfig struct {
+	Brokers []string `mapstructure:"KAFKA_BROKERS"`
+}
+
+func Load() (*Config, error) {
+	commonconfig.Init()
+
+	cfg := ToCfg()
+
+	brokers := viper.GetString("KAFKA_BROKERS")
+	if brokers != "" {
+		cfg.Kafka.Brokers = strings.Split(brokers, ",")
+	}
+
+	if err := commonconfig.RequireString("DSN", cfg.Dsn); err != nil {
+		return nil, err
+	}
+
+	if err := commonconfig.RequireString("HTTP_PORT", cfg.HTTPPort); err != nil {
+		return nil, err
+	}
+
+	if err := commonconfig.RequireString("GRPC_PORT", cfg.GRPCPort); err != nil {
+		return nil, err
+	}
+
+	if err := commonconfig.RequireMinLength("JWT_SECRET", cfg.JWTSecret, 32); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
---
+++ services/notification/config/helper.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+func ToCfg() *Config {
+	fmt.Println("ToCfg DSN:", viper.GetString("DSN"))
+
+	cfg := &Config{
+		Dsn:         viper.GetString("DSN"),
+		JWTSecret:   viper.GetString("JWT_SECRET"),
+		HTTPPort:    viper.GetString("HTTP_PORT"),
+		GRPCPort:    viper.GetString("GRPC_PORT"),
+		ServiceName: viper.GetString("SERVICE_NAME"),
+		Version:     viper.GetString("VERSION"),
+		EmailAPIKey: viper.GetString("EMAIL_API_KEY"),
+		AppEmail:    viper.GetString("APP_EMAIL"),
+		FromName:    viper.GetString("FROM_NAME"),
+		LogLevel:    viper.GetString("LOG_LEVEL"),
+		Redis: Redis{
+			Addr:     viper.GetString("REDIS_ADDR"),
+			Password: viper.GetString("REDIS_PASSWORD"),
+			DB:       viper.GetInt("REDIS_DB"),
+		},
+		Kafka: KafkaConfig{
+			Brokers: strings.Split(
+				viper.GetString("KAFKA_BROKERS"),
+				",",
+			),
+		},
+	}
+
+	fmt.Println("Assigned DSN:", cfg.Dsn)
+
+	return cfg
+}
