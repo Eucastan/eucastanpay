9238a6c9d1324afa0e10353f99acba3eef24f69f
Stanley Emeh <stanemeh808@gmail.com>
2026-07-12 23:42:37 +0100

    Shared configuration data

--- common/pkg/config/config.go
+++ common/pkg/config/config.go
@@ -1,6 +1,45 @@
 package config
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
+
+type SharedCfg struct {
+	Dsn       string `mapstructure:"DSN"`
+	Schema    string `mapstructure:"SCHEMA"`
+	JWTSecret string `mapstructure:"JWT_SECRET"`
+	Redis     Redis  `mapstructure:",squash"`
+	Kafka     KafkaConfig
+	LogLevel  string `mapstructure:"LOG_LEVEL"`
+}
+
+type Redis struct {
+	Addr     string `mapstructure:"REDIS_ADDR"`
+	Password string `mapstructure:"REDIS_PASSWORD"`
+	DB       int    `mapstructure:"REDIS_DB"`
+}
+
+type KafkaConfig struct {
+	Brokers  []string `mapstructure:"KAFKA_BROKERS"`
+	Username string   `mapstructure:"KAFKA_USERNAME"`
+	Password string   `mapstructure:"KAFKA_PASSWORD"`
+}
+
+func ValidateSchema(schema string) error {
+	switch schema {
+	case "user",
+		"account",
+		"ledger",
+		"transfer",
+		"audit",
+		"payment",
+		"admin":
+		return nil
+	default:
+		return fmt.Errorf("invalid schema: %s", schema)
+	}
+}
 
 func RequireString(name, value string) error {
 	if value == "" {
