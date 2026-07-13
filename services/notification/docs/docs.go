095ee01531e5ab21308c2997051a9505e86750cd
Stanley Emeh <stanemeh808@gmail.com>
2026-07-13 02:49:17 +0100

    swagger docs

--- services/notification/docs/docs.go
+++ services/notification/docs/docs.go
@@ -195,9 +195,9 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "eucastanpay.onrender.com",
+	Host:             "notification-xvaz.onrender.com",
 	BasePath:         "/api/v1",
-	Schemes:          []string{"http", "https"},
+	Schemes:          []string{"https"},
 	Title:            "EucastanPay Notification Service API",
 	Description:      "Notification Management Service for EucastanPay.",
 	InfoInstanceName: "swagger",
