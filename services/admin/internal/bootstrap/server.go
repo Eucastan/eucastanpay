c405e15a5466be04427bd81e35009fe28fece108
Stanley Emeh <stanemeh808@gmail.com>
2026-07-25 15:58:34 +0100

    typo fix

--- services/admin/internal/bootstrap/server.go
+++ services/admin/internal/bootstrap/server.go
@@ -7,7 +7,7 @@ import (
 
 func (a *App) initServer() {
 	a.server = &http.Server{
-		Addr:         a.cfg.HTTPPort,
+		Addr:         ":" + a.cfg.HTTPPort,
 		Handler:      a.router,
 		ReadTimeout:  15 * time.Second,
 		WriteTimeout: 15 * time.Second,
