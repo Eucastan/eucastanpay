088e31beeebd91d6616bea0141af528e3fcde098
Stanley Emeh <stanemeh808@gmail.com>
2026-07-25 17:27:02 +0100

    fixed typo in bootstrap

--- services/audit/internal/bootstrap/logger.go
+++ services/audit/internal/bootstrap/logger.go
@@ -3,6 +3,6 @@ package bootstrap
 import "github.com/Eucastan/eucastanpay/common/pkg/logger"
 
 func (a *App) initLogger() {
-	a.logger = logger.New(a.cfg.LogLevel)
+	a.logger = logger.New(a.cfg.SharedCfg.LogLevel)
 	a.logger.Info("Starting Audit service...")
 }
