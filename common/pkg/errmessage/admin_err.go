e811e8d78dde45e38d22a0dddfdea96bb3ab4764
Stanley Emeh <stanemeh808@gmail.com>
2026-07-29 02:12:59 +0100

    Added new error log

--- common/pkg/errmessage/admin_err.go
+++ common/pkg/errmessage/admin_err.go
@@ -3,6 +3,7 @@ package errmessage
 import "errors"
 
 var (
+	ErrForbidden       = errors.New("forbidden")
 	ErrAdminNotFound   = errors.New("admin not found")
 	Err2FANotEnabled   = errors.New("2factor authentication not enabled")
 	ErrInvalid2FACode  = errors.New("invalid 2factor authentication")
