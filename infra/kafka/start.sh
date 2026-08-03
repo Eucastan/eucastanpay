702136fbaacfe77ce6f1244382f9548c71b8b336
Stanley Emeh <stanemeh808@gmail.com>
2026-08-04 00:27:51 +0100

    kafka broker setup

---
+++ infra/kafka/start.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+CONFIG=/opt/kafka/config/server.properties
+DATA=/var/lib/kafka/data
+META=$DATA/meta.properties
+
+mkdir -p $DATA
+
+if [ ! -f "$META" ]; then
+    echo "Formatting Kafka storage..."
+
+    CLUSTER_ID=$(/opt/kafka/bin/kafka-storage.sh random-uuid)
+
+    /opt/kafka/bin/kafka-storage.sh format \
+        --ignore-formatted \
+        -t $CLUSTER_ID \
+        -c $CONFIG
+fi
+
+echo "Starting Kafka..."
+
+exec /opt/kafka/bin/kafka-server-start.sh $CONFIG
